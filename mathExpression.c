d10197b3ede2c289f63d2ae53f8cc75f4ea77f9b
nayon117 <hasibul.nayon1@gmail.com>
2024-10-16 23:01:47 +0600

    solve math expression problem

---
+++ mathExpression.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int main() {
+
+    int a, b, c;
+    char s, q;
+
+    scanf("%d %c %d %c %d", & a, & s, & b, & q, & c);
+
+    if (s == '+') {
+
+        if ((a + b) == c) {
+            printf("Yes\n");
+
+        } else {
+            printf("%d", a + b);
+
+        }
+
+    } else if (s == '-') {
+
+        if ((a - b) == c) {
+            printf("Yes\n");
+        } else {
+            printf("%d", a - b);
+        }
+
+    } else if (s == '*') {
+
+        if ((a * b) == c) {
+            printf("Yes\n");
+        } else {
+            printf("%d", a * b);
+        }
+
+    }
+
+}
